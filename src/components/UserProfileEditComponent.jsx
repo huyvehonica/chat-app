ffb98d7373e86cc38e2ccbfae91a62b27cee5e13
nnhuy <156220601+huykiraa@users.noreply.github.com>
2025-04-23 17:30:40 +0700

    edit profile

--- src/components/UserProfileEditComponent.jsx
+++ src/components/UserProfileEditComponent.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "./ui/button";
 import {
   Dialog,
@@ -10,8 +11,34 @@ import {
 } from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { ref, update } from "firebase/database";
+import { rtdb } from "../firebase/firebase";
+import toast from "react-hot-toast";
 
 export function UserProfileEditComponent({ isOpen, onClose, user }) {
+  const [fullName, setFullName] = useState(user?.fullName || "");
+  const [username, setUsername] = useState(user?.username || "");
+  const handleSaveChanges = async () => {
+    if (!user?.uid) {
+      console.error("User ID is not available.");
+      return;
+    }
+    if (!fullName || !username) {
+      toast.error("Please fill in all fields.");
+      return;
+    }
+    try {
+      const userRef = ref(rtdb, `users/${user?.uid}`);
+      await update(userRef, {
+        fullName,
+        username,
+      });
+      toast.success("Profile updated successfully!");
+      onClose(); // Close the modal after saving changes
+    } catch (error) {
+      console.error("Error updating user profile:", error);
+    }
+  };
   return (
     <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
       <DialogContent className="sm:max-w-[425px]">
@@ -26,17 +53,33 @@ export function UserProfileEditComponent({ isOpen, onClose, user }) {
             <Label htmlFor="name" className="text-right">
               Name
             </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            <Input
+              id="name"
+              value={fullName}
+              className="col-span-3 bg-[#01aa851d] text-[#004939f3]"
+              onChange={(e) => setFullName(e.target.value)}
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Username
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Input
+              id="username"
+              value={username}
+              className="col-span-3 bg-[#01aa851d] text-[#004939f3]"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button
+            className="bg-[#01aa85]"
+            type="submit"
+            onClick={handleSaveChanges}
+          >
+            Save changes
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
